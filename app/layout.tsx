f4f351e1eabe2f7eda1640e9b9c9e46767cbe3ee
Toby Hogan <tobyhogan44@gmail.com>
2024-02-27 12:34:52 +0000

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ import './globals.css'
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Spotify Clone',
-  description: 'Spotify Clone',
+  title: 'Meditazen',
+  description: 'Meditazen',
 }
 
 export const revalidate = 0;
